86ca21f9a3056c201d30c02a9887e88d6ed1f8e8
RobPruzan <rapruzan@buffalo.edu>
2025-06-19 20:48:57 -0700

    toggle for hydration

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -5,17 +5,29 @@ import BugRepro from "./BugRepro";
 
 const rootElement = document.getElementById("root")!;
 
-const html = ReactDOMServer.renderToString(
-  <React.StrictMode>
-    <BugRepro />
-  </React.StrictMode>
-);
+const HYDRATE = true;
 
-rootElement.innerHTML = html;
+// must be hydrated to encounter too many hooks error
+if (HYDRATE) {
+  const html = ReactDOMServer.renderToString(
+    <React.StrictMode>
+      <BugRepro />
+    </React.StrictMode>
+  );
 
-ReactDOM.hydrateRoot(
-  rootElement,
-  <React.StrictMode>
-    <BugRepro />
-  </React.StrictMode>
-);
+  rootElement.innerHTML = html;
+
+  ReactDOM.hydrateRoot(
+    rootElement,
+    <React.StrictMode>
+      <BugRepro />
+    </React.StrictMode>
+  );
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <BugRepro />
+    </React.StrictMode>
+  );
+}
